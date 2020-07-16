522d9b331b660aeddcba18e03f12d9848caaa92e
pr1266 <pourya_1999@yahoo.com>
2020-07-16 10:37:20 -0700

    ''

--- labels.py
+++ labels.py
@@ -2,19 +2,47 @@ from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel, MDIcon
 from kivymd.uix.boxlayout import MDBoxLayout
 
+
+class Component(MDBoxLayout):
+
+    def __init__(self, **kwargs):
+
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'    
+        self.add_widget(
+            MDLabel(
+                text = 'a message from Pr1266', 
+                halign = 'center', 
+                theme_text_color = 'Error',  
+                font_style = 'H6',
+            )
+        )
+
+        self.add_widget(
+            MDLabel(
+                text = 'a message from Pr1266', 
+                halign = 'center', 
+                theme_text_color = 'Primary',  
+                font_style = 'H1',
+            )
+        )
+
+        self.add_widget(
+            MDLabel(
+                text = 'a message from Pr1266', 
+                halign = 'center', 
+                theme_text_color = 'Secondary',  
+                font_style = 'H3',
+            )
+        )
+        
+        self.add_widget(MDIcon(icon = 'language-python', halign = 'center'))
+        
 class DemoApp(MDApp):
 
     def build(self):
 
-        label = MDLabel(
-            text = 'a message from Pr1266', 
-            halign = 'center', 
-            theme_text_color = 'Custom', 
-            text_color = (198/255.0, 98/255.0, 81/255.0, 1), 
-            font_style = 'H3',
-            )
-
-        icon_label = MDIcon(icon = 'language-python', halign = 'center')
-        return icon_label
+        comp = Component()
+        return comp
 
 DemoApp().run()
