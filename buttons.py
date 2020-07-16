57f71089e2a5d1586e05b6b23fc4f221af636010
pr1266 <pourya_1999@yahoo.com>
2020-07-16 13:08:01 -0700

    ''

--- buttons.py
+++ buttons.py
@@ -2,7 +2,7 @@ from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel, MDIcon
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.screen import Screen
-from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton, MDRaisedButton
+from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton, MDRaisedButton, MDRectangleFlatIconButton, MDFillRoundFlatIconButton
     
 
 class Component(MDFloatLayout):
@@ -14,28 +14,47 @@ class Component(MDFloatLayout):
         self.add_widget(
             MDFlatButton(
             text = 'flat button', 
-            pos_hint = {'center_x': 0.5, 'center_y': 0.2}
+            pos_hint = {'center_x': 0.5, 'center_y': 0.1}
             )
         )
 
         self.add_widget(
+            MDRectangleFlatIconButton(
+            text = 'rectangle flat icon button',
+            increment_width = 500,
+            icon = 'android',
+            #text_halign = 'center',
+            pos_hint = {'center_x': 0.5, 'center_y': 0.2}
+            )
+        )
+
+        # self.add_widget(
+        #     MDFillRoundFlatIconButton(
+        #     text = 'fill round flat icon button',
+        #     icon = 'languages-python',
+        #     #text_halign = 'center',
+        #     pos_hint = {'center_x': 0.5, 'center_y': 0.5}
+        #     )
+        # )
+
+        self.add_widget(
             MDRaisedButton(
             text = 'Raised button', 
-            pos_hint = {'center_x': 0.5, 'center_y': 0.4}
+            pos_hint = {'center_x': 0.5, 'center_y': 0.3}
             )
         )
 
         self.add_widget(
             MDRectangleFlatButton(
             text = 'rectangle flat button', 
-            pos_hint = {'center_x': 0.5, 'center_y': 0.6}
+            pos_hint = {'center_x': 0.5, 'center_y': 0.4}
             )
         )
 
         self.add_widget(
             MDIconButton(
             icon = 'android',
-            pos_hint = {'center_x': 0.5, 'center_y': 0.8}
+            pos_hint = {'center_x': 0.5, 'center_y': 0.5}
             )
         )
 
---
+++ card.py
@@ -0,0 +1,79 @@
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivymd.app import MDApp
+from kivymd.uix.card import MDCardSwipe
+
+KV = '''
+<SwipeToDeleteItem>:
+    size_hint_y: None
+    height: content.height
+
+    MDCardSwipeLayerBox:
+        padding: "8dp"
+
+        MDIconButton:
+            icon: "trash-can"
+            pos_hint: {"center_y": .5}
+            on_release: app.remove_item(root)
+
+    MDCardSwipeFrontBox:
+
+        OneLineListItem:
+            id: content
+            text: root.text
+            _no_ripple_effect: True
+
+
+Screen:
+    canvas.before:
+        Color:
+            rgba: (1, 1, 1, 1)
+        Rectangle:
+            pos: self.pos
+            size: self.size
+    BoxLayout:
+        orientation: "vertical"
+        spacing: "10dp"
+
+        MDToolbar:
+            canvas.before:
+                Color:
+                    rgba: (1, 0, 1, 1)
+                Rectangle:
+                    pos: self.pos
+                    size: self.size
+            elevation: 10
+            title: "Pr1266"
+            color: (1, 1, 0, 1)
+
+        ScrollView:
+
+            MDList:
+                id: md_list
+                padding: 0
+'''
+
+
+class SwipeToDeleteItem(MDCardSwipe):
+    text = StringProperty()
+
+
+class TestCard(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.screen = Builder.load_string(KV)
+        self.theme_cls.theme_style = "Dark"
+
+    def build(self):
+        return self.screen
+
+    def remove_item(self, instance):
+        self.screen.ids.md_list.remove_widget(instance)
+
+    def on_start(self):
+        for i in range(20):
+            self.screen.ids.md_list.add_widget(
+                SwipeToDeleteItem(text = f"item {i}")
+            )
+
+TestCard().run()
